722afb85dbf788988ce906ae31f23d5a6fc2bbfd
Santhosh M R <santhoshmr@Santhoshs-MacBook-Air.local>
2025-10-26 19:47:53 +0530

    ignite app

--- App.js
+++ App.js
@@ -1,36 +1,37 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 const heading = React.createElement("h1", { id: "heading" }, "Hello world");
 
 // creating parent, child and h1
-const parent1 = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement(
-    "div",
-    { id: "child" },
-    React.createElement("h1", {}, "this is h1 tag")
-  )
-);
+// const parent1 = React.createElement(
+//   "div",
+//   { id: "parent" },
+//   React.createElement(
+//     "div",
+//     { id: "child" },
+//     React.createElement("h1", {}, "this is h1 tag")
+//   )
+// );
 
-// creating parent, child -> h1 and h2
-const parent2 = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "this is h1 tag"),
-    React.createElement("h2", {}, "this is h2 tag"),
-  ])
-);
+// // creating parent, child -> h1 and h2
+// const parent2 = React.createElement(
+//   "div",
+//   { id: "parent" },
+//   React.createElement("div", { id: "child" }, [
+//     React.createElement("h1", {}, "this is h1 tag"),
+//     React.createElement("h2", {}, "this is h2 tag"),
+//   ])
+// );
 
 // create parent, child1 -> h1,h2, child 2 -> h1,h2
-
 const parent3 = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", {}, "this is h1 tag"),
-    React.createElement("h2", {}, "this is h2 tag"),
+  React.createElement("div", { id: "child1", key: "div1" }, [
+    React.createElement("h1", { key: "1" }, "this is h1 tag"),
+    React.createElement("h2", { key: "2" }, "this is h2 tag"),
   ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "this is h1 tag"),
-    React.createElement("h2", {}, "this is h2 tag"),
+  React.createElement("div", { id: "child2", key: "div2" }, [
+    React.createElement("h1", { key: "3" }, "this is h1 tag"),
+    React.createElement("h2", { key: "4" }, "this is h2 tag"),
   ]),
 ]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-namaste react
+# namaste react
+
+# parcel
+
+- Dev Build
+- Local Server
+- HMR = Hot Module Replacement
+- File Watching Algorithm - written in c++
+- Caching - Faster Builds
+- Image Optimization
+- Minification
+- Bundling
+- Compress
+- Consistent Hashing
+- Code Splitting
+- Differential Bundling - support older browser
+- Diagnostic
+- Error Handling
+- HTTPs
+- Tree shaking
+- Different dev and prod bundles
